79323bd005d2484e8e6bfa73f5401bfdcec2dd5c
Hun5LEE <jhlee091697@gmail.com>
2023-03-20 22:28:22 +0900

    first commit

---
+++ src/Components/Header.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import "../ComponentsCss/Header.css";
+import SearchIcon from "@mui/icons-material/Search";
+
+function Header() {
+  return (
+    <div className="header">
+      {/* 로고 */}
+      <img
+        className="header_logo"
+        src={process.env.PUBLIC_URL + "./Img/logo.png"}
+      />
+      {/* 검색창 */}
+      <div className="header_search">
+        <input className="header_searchInput" type="text" />
+        <SearchIcon className="header_searchIcon" />
+      </div>
+      {/* 헤더 네비게이션 */}
+      <div className="header_nav">
+        <div className="header_option">
+          <span className="header_optionLineOne">안녕하세요</span>
+          <span className="header_optionLineTwo">로그인하기</span>
+        </div>
+        <div className="header_option">
+          <span className="header_optionLineOne">돌아가기</span>
+          <span className="header_optionLineTwo">주문내역</span>
+        </div>
+        <div className="header_option">
+          <span className="header_optionLineOne">반가워요</span>
+          <span className="header_optionLineTwo">구독과좋아요</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBW5WKOsq3prYZnchYjBItSWHiHpz5Sli4",
+  authDomain: "react-shoppingmall-4c8e9.firebaseapp.com",
+  projectId: "react-shoppingmall-4c8e9",
+  storageBucket: "react-shoppingmall-4c8e9.appspot.com",
+  messagingSenderId: "37490853961",
+  appId: "1:37490853961:web:919614707d943203c2d798",
+  measurementId: "G-EZ72XX33PP",
+};
